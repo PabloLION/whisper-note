6dca53be1de91b663ff9373aca7e921b0c969e15
PabloLION <36828324+PabloLION@users.noreply.github.com>
2023-10-06 02:27:58 +0200

    ref: reduce cognitive difficulty

--- transcribe_demo.py
+++ transcribe_demo.py
@@ -1,5 +1,3 @@
-#! python3.7
-
 import argparse
 import io
 import os
@@ -64,25 +62,25 @@ def build_args() -> argparse.Namespace:
 
 
 def load_microphone_source(args) -> Result[sr.Microphone, str]:
-    # Important for linux users.
-    # Prevents permanent application hang and crash by using the wrong Microphone
-    if "linux" in platform:
-        mic_name = args.default_microphone
-        if not mic_name or mic_name == "list":
-            print("Available microphone devices are: ")
-            for index, name in enumerate(sr.Microphone.list_microphone_names()):
-                print(f'Microphone with name "{name}" found')
-            return Err("No microphone name provided, aborting.")
-        else:
-            for index, name in enumerate(sr.Microphone.list_microphone_names()):
-                if mic_name in name:
-                    source = sr.Microphone(sample_rate=16000, device_index=index)
-                    break
-            else:
-                print(f"No microphone with name {mic_name} found, aborting.")
-                return Err("No microphone with name {mic_name} found, aborting.")
-    else:
+    if "linux" not in platform:
         source = sr.Microphone(sample_rate=16000)
+        return Ok(source)
+
+    # Prevents permanent application hang and crash by using the wrong
+    # Microphone for Linux users.
+    mic_name = args.default_microphone
+    if not mic_name or mic_name == "list":
+        print("Showing available microphone devices: ")
+        for index, name in enumerate(sr.Microphone.list_microphone_names()):
+            print(f'Found microphone with default microphone name "{name}"')
+        return Err("No microphone name provided, aborting.")
+    else:
+        for index, name in enumerate(sr.Microphone.list_microphone_names()):
+            if mic_name in name:
+                source = sr.Microphone(sample_rate=16000, device_index=index)
+                break
+        else:
+            return Err("Default microphone with name {mic_name} not found.")
     return Ok(source)
 
 
@@ -95,7 +93,6 @@ def initialize_source_recorder_with_queue(
     recorder.energy_threshold = args.energy_threshold
     # Definitely do this, dynamic energy compensation lowers the energy threshold dramatically to a point where the SpeechRecognizer never stops recording.
     recorder.dynamic_energy_threshold = False
-
     source = load_microphone_source(args).or_else(lambda err: exit(err)).unwrap()
 
     with source:
@@ -106,10 +103,9 @@ def initialize_source_recorder_with_queue(
         Threaded callback function to receive audio data when recordings finish.
         audio: An AudioData containing the recorded bytes.
         """
-        # Grab the raw bytes and push it into the thread safe queue.
         data = audio.get_raw_data()
+        data_queue.put(data)  # push the raw bytes to the thread safe queue.
         print(f"Received {len(data)} bytes of audio data.")
-        data_queue.put(data)
 
     # Create a background thread that will pass us raw audio bytes.
     # We could do this manually but SpeechRecognizer provides a nice helper.
