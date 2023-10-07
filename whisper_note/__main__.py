f10cc357374f64ec0e1b4a0790042d3fa0a2c8e4
PabloLION <36828324+PabloLION@users.noreply.github.com>
2023-10-07 23:55:13 +0200

    chore: remove textual files

--- whisper_note/__main__.py
+++ whisper_note/__main__.py
@@ -1,10 +1,5 @@
 from whisper_note.transcriber import Transcriber
 from whisper_note.parse_env_cfg import CONFIG
-from whisper_note.tui import run_textual_ui
-
-
-def run_tui():
-    run_textual_ui()
 
 
 def run_core():
@@ -15,8 +10,8 @@ def run_core():
 
 def run():
     # hooked to poetry run whisper-note, poetry run note
-    run_tui()
+    run_core()
 
 
 if __name__ == "__main__":
-    run_textual_ui()
+    run()
