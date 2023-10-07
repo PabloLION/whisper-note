f616e4e8490a089700fde3898b340234243fa3a4
PabloLION <36828324+PabloLION@users.noreply.github.com>
2023-10-07 17:58:39 +0200

    chore: config pre-commit

--- tests/__main__.py
+++ tests/__main__.py
@@ -6,8 +6,7 @@ import pytest
 def run_all_tests():
     """Run all tests in the tests/ directory."""
     print("Running all tests...")
-    pytest.main(["."])
-    # sys.exit(pytest.main(["-x", "tests"]))
+    sys.exit(pytest.main(["-x", "tests"]))
 
 
 if __name__ == "__main__":
