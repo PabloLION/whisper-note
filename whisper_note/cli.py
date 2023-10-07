6d64a9bc81be72aff8b7fbe0292d09ad838e650d
PabloLION <36828324+PabloLION@users.noreply.github.com>
2023-10-08 01:29:03 +0200

    feat: add rich table example

--- whisper_note/cli.py
+++ whisper_note/cli.py
@@ -1,5 +1,8 @@
 import argparse
 from sys import platform
+from typing import Final
+from rich.console import Console
+from rich.table import Table
 
 
 def build_default_args() -> argparse.Namespace:
@@ -37,3 +40,55 @@ def build_default_args() -> argparse.Namespace:
         )
     args = parser.parse_args()
     return args
+
+
+class RichTable:
+    console: Console
+    table: Table
+
+    @property
+    def screen_width(self) -> int:
+        return self.console.width
+
+    def __init__(self) -> None:
+        self.console = Console()
+
+        table = Table(
+            show_header=True,
+            header_style="bold magenta",
+            title="Transcripts",
+            expand=True,
+            show_lines=True,
+            show_edge=False,
+        )
+        time_width = 11
+        checker_width = 4
+        text_width = self.console.width - time_width - checker_width * 2 - 3 * 5
+        table.add_column(
+            "Time",
+            style="dim",
+            justify="center",
+            vertical="middle",
+            width=time_width,
+        )
+        table.add_column(
+            "Text & Translation", justify="left", width=text_width, overflow="fold"
+        )
+        table.add_column("EN", justify="center", width=checker_width)
+        table.add_column("CN", justify="center", width=checker_width)
+
+        table.add_row(
+            "00:00:00:000",
+            "Hello, world!\n你好，世界！",
+            "✓",
+            "_",
+        )
+        table.add_row(
+            "00:00:00:000",
+            "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco\n你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！",
+            "✓",
+            "_",
+        )
+
+    def print(self) -> None:
+        self.console.print(self.table)
