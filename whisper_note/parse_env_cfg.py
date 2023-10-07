c71570ec803c5f57c6d06a9141cfe62d36478102
PabloLION <36828324+PabloLION@users.noreply.github.com>
2023-10-07 02:34:44 +0200

    ref: wrap parse_env_and_config for test

--- whisper_note/parse_env_cfg.py
+++ whisper_note/parse_env_cfg.py
@@ -10,31 +10,6 @@ class ConfigLoadingError(ValueError):
     pass
 
 
-dotenv.load_dotenv()  # #TODO: use dynamic env vars for test
-cfg_path = os.path.abspath(__file__ + "/../../config.yml")
-parsed_cfg = {}
-
-with open(cfg_path) as cfg_file:
-    cfg = yaml.safe_load(cfg_file)
-    parsed_cfg["model"] = cfg.get("model", "small")
-    parsed_cfg["translator"] = cfg.get("translator", "NONE")
-    parsed_cfg["source_lang"] = Language(cfg.get("source_lang", "English"))
-    parsed_cfg["target_lang"] = Language(cfg.get("target_lang", "Chinese_Simplified"))
-
-
-# parse translator api key
-# match parsed_cfg["translator"] : # #TODO: use this for python 3.10+
-if parsed_cfg["translator"] == "NONE":
-    parsed_cfg["translator_key"] = ""
-if parsed_cfg["translator"] == "DEEPL":
-    key = os.getenv("DEEPL_API_KEY")
-    if not key:
-        raise ConfigLoadingError("DEEPL_API_KEY is not set in .env file")
-    parsed_cfg["translator_key"] = key
-else:
-    raise ConfigLoadingError(f"Unknown translator: {parsed_cfg['translator'] =}")
-
-
 class FrozenConfig(NamedTuple):
     translator: str
     translator_key: str
@@ -43,8 +18,37 @@ class FrozenConfig(NamedTuple):
     target_lang: Language
 
 
-CONFIG = FrozenConfig(**parsed_cfg)
-parsed_cfg.clear()
+def parse_env_and_config() -> FrozenConfig:
+    dotenv.load_dotenv()  # #TODO: use dynamic env vars for test
+    cfg_path = os.path.abspath(__file__ + "/../../config.yml")
+    parsed_cfg = {}
+
+    with open(cfg_path) as cfg_file:
+        cfg = yaml.safe_load(cfg_file)
+        parsed_cfg["model"] = cfg.get("model", "small")
+        parsed_cfg["translator"] = cfg.get("translator", "NONE")
+        parsed_cfg["source_lang"] = Language(cfg.get("source_lang", "English"))
+        parsed_cfg["target_lang"] = Language(
+            cfg.get("target_lang", "Chinese_Simplified")
+        )
+
+    # parse translator api key
+    # match parsed_cfg["translator"] : # #TODO: use this for python 3.10+
+    if parsed_cfg["translator"] == "NONE":
+        parsed_cfg["translator_key"] = ""
+    if parsed_cfg["translator"] == "DEEPL":
+        key = os.getenv("DEEPL_API_KEY")
+        if not key:
+            raise ConfigLoadingError("DEEPL_API_KEY is not set in .env file")
+        parsed_cfg["translator_key"] = key
+    else:
+        raise ConfigLoadingError(f"Unknown translator: {parsed_cfg['translator'] =}")
+    CONFIG = FrozenConfig(**parsed_cfg)
+    parsed_cfg.clear()
+    return CONFIG
+
+
+CONFIG = parse_env_and_config()
 
 
 def test_parse_env_and_config():
