8d7d6bee8751f0f1dcf20ff4d076c2d0e0345d8c
PabloLION <36828324+PabloLION@users.noreply.github.com>
2023-10-08 02:45:37 +0200

    test: add template for rich table

---
+++ tests/rich_table_test.py
@@ -0,0 +1,22 @@
+from rich.table import Table
+
+table = Table(
+    show_header=True,
+    header_style="bold magenta",
+    title="Transcripts",
+    expand=True,
+    show_lines=True,
+    show_edge=False,
+)
+table.add_row(
+    "00:00:00:000",
+    "Hello, world!\n你好，世界！",
+    "✓",
+    "_",
+)
+table.add_row(
+    "00:00:00:000",
+    "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco\n你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！你好，世界！",
+    "✓",
+    "_",
+)
