2b36be79147135bf3fc6f8304c0a00799c5b4ef9
PabloLION <36828324+PabloLION@users.noreply.github.com>
2023-10-08 18:07:53 +0200

    feat: manual impl of FrozenConfig type checking

--- whisper_note/supportive_class/typed_config.py
+++ whisper_note/supportive_class/typed_config.py
@@ -6,17 +6,17 @@ class InvalidConfigError(ValueError):
     pass
 
 
-# I don't know how to gather the types of values dynamically
+# I don't know how to get a data
 ConfigValue = str | Language | int | None
 
 
-@final  # #TODO: use dataclass
+@final
 class FrozenConfig(NamedTuple):
     dot_env_path: str
     translator: str
     translator_env_key: str
     model: str
-    source_lang: Language
+    source_lang: Language | None  # both translator and whisper support None
     target_lang: Language
     linux_microphone: str | None
     energy_threshold: int
@@ -26,7 +26,29 @@ class FrozenConfig(NamedTuple):
     merged_transcription: str
 
     def mutated_copy(self: "FrozenConfig", **kwargs: ConfigValue) -> "FrozenConfig":
-        return FrozenConfig(**{**self._asdict(), **kwargs})
+        nf = FrozenConfig(**{**self._asdict(), **kwargs})  # type: ignore
+        nf.__type_check__()  # do the type check due to type ignore above
+        return nf
+
+    def __type_check__(self) -> None:
+        expected_type = {
+            "dot_env_path": str,
+            "translator": str,
+            "translator_env_key": str,
+            "model": str,
+            "source_lang": Language | None,
+            "target_lang": Language,
+            "linux_microphone": str | None,
+            "energy_threshold": int,
+            "phrase_max_second": int,
+            "summarizer": str,
+            "store_merged_wav": str,
+            "merged_transcription": str,
+        }
+        for k, v in self._asdict().items():
+            if isinstance(v, expected_type[k]):
+                continue
+            raise InvalidConfigError(f"Invalid config value type: {type(v)} for {k}")
 
 
 EXAMPLE_CONFIG = FrozenConfig(
